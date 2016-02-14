c5bcb683e857c394513601b4a19c1b53acc2f2e7
Attis Bijlani <attisb@gmail.com>
2016-02-14 18:45:18 -0500

    Bugs

--- app/controllers/backend/teams_controller.rb
+++ app/controllers/backend/teams_controller.rb
@@ -7,7 +7,7 @@ class Backend::TeamsController < ApplicationController
       @teams = Team.where('name LIKE :search', search: params[:s]).joins(:scores).paginate(:page => params[:page]).order('scores.points desc')
       #@teams = Team.where('first_name LIKE :search', search: params[:s]).joins(:scores).paginate(:page => params[:page]).group('scores.team_id').order('scores.points desc')
     else
-      @teams = Team.joins(:scores)select("teams.*, sum(scores.points) as average_rating").group("teams.id").order("average_rating DESC")
+      @teams = Team.joins(:scores).select("teams.*, sum(scores.points) as average_rating").group("teams.id").order("average_rating DESC")
       #@teams = Team.joins(:scores).paginate(:page => params[:page]).group('scores.team_id').order('scores.points desc').all
       #@teams = Team.all.joins(:scores).paginate(:page => params[:page]).group('scores.team_id').order('scores.points desc')
     end
