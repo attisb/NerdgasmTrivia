0fd8eefcf05f7e80bdd47dec12174130a8525119
Attis Bijlani <attisb@gmail.com>
2016-02-14 00:36:12 -0500

    Fix email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'help@nerdgasmtrivia.com'
+  config.mailer_sender = 'Nerdgasm Trivia <help@nerdgasmtrivia.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
