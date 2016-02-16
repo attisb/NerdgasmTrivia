04708e222db892c0fe9c1d557b5b5a1d788c0a83
Attis Bijlani <attisb@gmail.com>
2016-02-16 17:34:32 -0500

    Avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 #    else
       default_url = "#{root_url}i/default_avatar.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=400&d=#{CGI.escape(default_url)}"
 #    end
   end
 end
