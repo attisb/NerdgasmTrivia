902a9e2c393eac55011b87dd86efad25d4b6e6dd
Attis Bijlani <attisb@gmail.com>
2016-03-03 18:47:22 -0500

    Bug fixes

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,7 @@
 class Score < ActiveRecord::Base
   belongs_to :event
   belongs_to :team
+  belongs_to :user
   scope :team_count, -> { distinct.count(:team_id) }
   scope :player_count, -> { distinct.count(:user_id) }
   scope :current_score, -> { order(points: :desc).first.points } #sum(:points)
